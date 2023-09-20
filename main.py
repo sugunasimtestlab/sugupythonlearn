de5d7d3ca65fc9a189459cb0f8a60b50b5c27fa8
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-20 06:20:20 +0000

    python casting  float type

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-#convert from int to float:
 x = float(1)
-
-#convert from float to int:
-y = int(2.8)
-
-#convert from int to complex:
-z = complex(1)
-
-
-
+y = float(2.8)
+z = float("3")
 print(x)
 print(y)
 print(z)
-
-print(type(x))
-print(type(y))
-print(type(z))
