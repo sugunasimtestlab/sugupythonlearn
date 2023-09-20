658f99a044dd93ce22d43ae34d1b508e3117fb74
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-20 07:58:05 +0000

    strings format

--- main.py
+++ main.py
@@ -13,6 +13,41 @@ print(b[-5:-2])
 b = "Hello, World!"
 print(b[-13:-1])
 
+a = "Hello, Moon!"
+print(a.upper())
+
+a = " Hi, Dear! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+a = "Hello, World!"
+
+a = " Nice , Flower!"
+b = a.split(",")
+print(b)
+
+a = "Sun"
+b = "Set"
+c = a + b
+print(c)
+
+age = 23
+txt = "My name is suguna, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price)) 
+
+
+
+
+
+
+
 
 
 
