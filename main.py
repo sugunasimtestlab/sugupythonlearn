f4605c3a9f0d92cfa20a9f371e3be10eb80f4322
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-20 05:06:00 +0000

    python numbers

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-x = 20.5
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
 
-#display x:
-print(x)
+#convert from int to float:
+a = float(x)
 
-#display the data type of x:
-print(type(x)) 
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
 
 
