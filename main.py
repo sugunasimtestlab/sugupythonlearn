55ca9b439ab04df78322cdec97625e5e0da2fa89
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-21 07:34:41 +0000

    bool True and False

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-a = 200
-b = 33
-
-if b > a:
-  print("b is greater than a")
-else:
-  print("b is not greater than a")
-
-
-  print(bool("Hello"))
-print(bool(15))
-
-
-class myclass():
-  def __len__(self):
-    return 0
-
-myobj = myclass()
-print(bool(myobj))
-
-
 def myFunction() :
   return True
 
-print(myFunction())
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+print(bool("birds"))
+print(bool("flower"))
+print(bool("schools"))
 
 
