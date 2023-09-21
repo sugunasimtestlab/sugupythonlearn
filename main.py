f71bbd1c90b6e2576f7e123d0ecb39f757ea9aa7
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-21 07:18:22 +0000

    python booleans

--- main.py
+++ main.py
@@ -1,58 +1,27 @@
-b = "Hello, World!"
-print(b[2:5])
+a = 200
+b = 33
 
-b = "Hello, World!"
-print(b[2:9])
-
-b = "Hello, World!"
-print(b[0:13])
-
-b = "Hello, World!"
-print(b[-5:-2])
-
-b = "Hello, World!"
-print(b[-13:-1])
-
-a = "Hello, Moon!"
-print(a.upper())
-
-a = " Hi, Dear! "
-print(a.strip())
-
-a = "Hello, World!"
-print(a.replace("H", "J"))
-a = "Hello, World!"
-
-a = " Nice , Flower!"
-b = a.split(",")
-print(b)
-
-a = "Sun"
-b = "Set"
-c = a + b
-print(c)
-
-age = 23
-txt = "My name is suguna, and I am {}"
-print(txt.format(age))
-
-quantity = 3
-itemno = 567
-price = 49.95
-myorder = "I want {} pieces of item {} for {} dollars."
-print(myorder.format(quantity, itemno, price)) 
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
 
 
+  print(bool("Hello"))
+print(bool(15))
 
 
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
 
 
+def myFunction() :
+  return True
 
-
-
-
-
-
-
+print(myFunction())
 
 
