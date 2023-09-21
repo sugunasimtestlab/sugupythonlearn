7129ceae38c5ccd3a5180e0542f69ec2f34d9d44
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-21 11:55:12 +0000

    add list items

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-
-thislist = ["apple", "banana", "cherry"]
-print(len(thislist))
-
-list1 = ["apple", "banana", "cherry"]
-list2 = [1, 5, 7, 9, 3]
-list3 = [True, False, False]
-
-print(list1)
-print(list2)
-print(list3)
-
-x = list(("apple", "banana", "cherry"))
-print(x)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4] )
 
 
 thislist = ["apple", "banana", "cherry"]
-print(thislist[-1])
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist) 
+
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist) 
+
+
+
+
 
 
