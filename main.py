7e270e504a9c993a2abfccaebf3525755cc0599a
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-22 06:42:16 +0000

    python tuples Tuple Items - Data Types

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
 
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(mylist)
+thistuple = ("apple",)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))
 
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
-
-list1 = ["a", "b", "c"]
-list2 = [1, 2, 3]
-list3 = list1 + list2
-print(list3)
-
-list1 =["suguna"]
-list2 =["sundarajothi"]
-list3 = list1+ list2
-print(list3)
+tuple1 = ("apple" "banana" "cherry")
+tuple2 = (10, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1)
+print(tuple2)
+print(tuple3)
+print(type(tuple1))
+print(type(tuple2))
 
 
