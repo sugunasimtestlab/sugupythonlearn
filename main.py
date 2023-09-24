334c2e69d86246c593865b84d2cbbb3837f16c6a
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-24 05:36:13 +0000

    Python Dictionary Methods

--- main.py
+++ main.py
@@ -1,51 +1,101 @@
-x = ("apple", "banana", "cherry")
-y = list(x)
-y[1] = "kiwi"
-x = tuple(y)
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
 print(x)
 
-thistuple = ("apple", "banana", "cherry")
-y = list(thistuple)
-y.append("orange")
-thistuple = tuple(y)
-print(thistuple)
 
-thistuple = ("apple", "banana", "kiwi")
-y = ("orange",)
-thistuple += y
-print(thistuple)
-
-
-thistuple = ("apple", "banana", "cherry")
-y = list(thistuple)
-y.remove("apple")
-thistuple = tuple(y)
-print(thistuple)
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print(x) #before the change
+car["color"] = "white"
+print(x) #after the change
 
 
-thistuple = ("tamil", "banana", "cherry")
-for x in thistuple:
-  print(x)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
 
-thistuple = ("math", "banana", "cherry")
-for i in range(len(thistuple)):
-  print(thistuple[i])
 
-  thistuple = ("one", "banana", "cherry")
-i = 0
-while i < len(thistuple):
-  print(thistuple[i])
-  i = i + 1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
 
-a=int(input())
-b=int(input())
-c=int(input())
-a=b+c 
-d=a*b*c 
-c=a/b 
-print(d)
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily)
 
-tuple1 = ("a", "b" , "c")
-tuple2 = (1, 2, 3)
-tuple3 = tuple1 + tuple2
-print(tuple3)
+
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(myfamily)
+
+
+
+
+
+
+
+
+
