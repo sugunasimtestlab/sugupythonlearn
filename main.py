d590e725d6a5b97d51ac199b37ee23e6180f2977
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-27 04:10:41 +0000

    while loop and for loops

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ while i < 6:
   print(i)
   i += 1
 
-  
 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x) 
+
+  
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+for x in range(2, 6):
+  print(x)
+
+
+  
