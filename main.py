0a399bc3b7cc182f63fd4273591b6efbdaefe778
suguna <145107073+sugunasimtestlab@users.noreply.github.com>
2023-09-27 04:36:10 +0000

    Python Functions
    tri_recursion(k):

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-def my_function(fname):
-  print(fname + " Refsnes")
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
 
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
-
-def my_function(fname, lname):
-  print(fname + " " + lname)
-
-my_function("Emil", "Refsnes")
-
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
-
-my_function("Emil", "Tobias", "Linus")
-def my_function(country = "Norway"):
-  print("I am from " + country)
-
-my_function("Sweden")
-my_function("India")
-my_function()
-my_function("Brazil")
+print("\n\nRecursion Example Results")
+tri_recursion(6)
